273789b81a5922a9e9ea0a009cdbf0e3dff1ee5d
Maung Bwar <aungbwar@outlook.com>
2026-03-28 14:28:23 -0700

    Version 1

---
+++ Example.java
@@ -0,0 +1,15 @@
+public class Example {
+  public static void main(String[] args) {
+
+    int x = 10;
+    int y = 100;
+
+    System.out.println(addMethod(x, y));
+    System.out.println("Version 1");
+
+  }
+
+  public static int addMethod (int x, int y) {
+    return x + y;
+  }
+}
---
+++ Recursive.java
@@ -0,0 +1,11 @@
+public class Recursive {
+    public static void main(String[] args) throws Exception {
+        System.out.println(factorial(5));
+        System.out.println("Version 1");
+    }
+
+    public static int factorial(int x ) {
+        if (x == 1) {return x; }
+        return factorial(x - 1) * x;
+    }
+}
