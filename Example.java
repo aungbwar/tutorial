f9ea5bcc496eef367719e5efdcb4fdc40b1fa11d
Maung Bwar <aungbwar@outlook.com>
2026-03-28 14:31:30 -0700

    Version 4

--- Example.java
+++ Example.java
@@ -5,7 +5,7 @@ public class Example {
     int y = 100;
 
     System.out.println(addMethod(x, y));
-    System.out.println("Version 3");
+    System.out.println("Version 4");
 
   }
 
--- Recursive.java
+++ Recursive.java
@@ -1,7 +1,7 @@
 public class Recursive {
     public static void main(String[] args) throws Exception {
         System.out.println(factorial(5));
-        System.out.println("Version 3");
+        System.out.println("Version 4");
     }
 
     public static int factorial(int x ) {
